0c2e70abad54cea870f747ebeb37c5ee057636cf
Kirill Arbuzov <arbuzov.kirill@gmail.com>
2020-03-10 22:56:23 +0200

    maven added

---
+++ src/main/java/com/company/TestClass.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class TestClass {
+    private int result;
+    private String error;
+//    private RemouteFile aaa;
+    private Map<String, Set<RemouteFile>> keywords;
+//    private Map<String, List<RemouteFile>> keywords2;
+//    private Map<String, Map<String, List<RemouteFile>>> keywords3;
+//    private Set<List<String>> collection;
+//    private Set<Set<String>> collection4;
+//    private List<List<String>> collection5;
+//    private Set<String> collection2;
+//    private List<String> collection3;
+}
